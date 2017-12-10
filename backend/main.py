878e4c6f4ed2660a9a7a5196a348d6b6138168a7
dvec <dvecend@gmail.com>
2017-12-10 22:00:57 +0400

    Changing server port to 80

--- backend/main.py
+++ backend/main.py
@@ -4,4 +4,4 @@ from controller import Controller
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    Controller.run(host='0.0.0.0', port=8081)
+    Controller.run(host='0.0.0.0', port=80)
